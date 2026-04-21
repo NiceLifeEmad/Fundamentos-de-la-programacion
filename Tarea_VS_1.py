8c7ca833c3514fd2be1d64b679e9cdeb578f3eb6
NiceLifeEmad <vice.inostroza.m@gmail.com>
2026-04-21 00:10:04 -0400

    Tarea completa

---
+++ Tarea_VS_1.py
@@ -0,0 +1,9 @@
+num=int(input("Ingrese un numero: "))
+if num>0:
+    num2=int(input("Ingrese otro numero: "))
+    if num2>0:
+        print (f"La suma de ambos numeros es: {num+num2}")
+    else:
+        print ("Uno de los numeros es negativo")
+else:
+    print ("El numero es negativo")
---
+++ for.py
@@ -0,0 +1,109 @@
+# Explicacion y uso de "for"
+
+# for i in range(7):
+#     print("hola")
+
+# num=int(input("Ingrese un numero: "))
+
+# for i in range(num):
+#      print(i+1, "repeticion")
+# print("segunda forma")
+# for i in range(1,num+1):
+#      print(i, "repeticion")
+
+
+# Pedir un numero al usuario y mostrar su tabla de multiplicar
+
+# num=int(input("Ingrese un numero: "))
+
+# #primera forma
+# for i in range(10):
+#       print(num, "x", i, "=", num*i)
+
+# #con manipulacion de limites
+# num=int(input("Ingrese un numero: "))
+
+# for i in range(1,11):
+#       print(num, "x", i, "=", num*i)
+
+# #usando f string
+# num=int(input("Ingrese un numero: "))
+
+# for i in range(1,11):
+#       print(f"{num}x{i}={num*i})
+
+# titulo="clima de hoy" #string letras
+# diaDelMes=17  #int numeros enteros
+# mes=4 #int numeros enteros
+# tem=25.7  #float numeros decimales
+# llueve=True  #boolean verdadero o falso
+# print(titulo, "temperatura: ", tem,diaDelMes,"/",mes)
+# print(f"{titulo} temperatura: {tem} {diaDelMes}/{mes}")
+
+# EJ
+# tem>28 ------->False
+# mes==4 ------>True
+
+# if llueve:
+#     print("Saca el paraguas")
+# else:
+#     print("Puede andar en polera")
+
+
+
+#Preguntar cuantas notas son y
+#sacar el promedio de ellas
+
+# notas=int(input("Cuantas notas son?: "))
+# prom=0
+# for i in range(notas):
+#        print("Ingrese una nota: ")
+#        sumN=float(input())
+#        prom=sumN+prom
+#     #    print(prom)
+
+# print("El promedio es:", round((prom/notas),1))
+
+#sumatoria
+# sumatoria=int(input("Ingreas un numero: "))
+# total=0
+# for i in range(sumatoria):
+#        total=total+i+1
+# print("La sumatoria es: ", total)
+
+
+#Factorial
+
+# sumatoria=int(input("Ingreas un numero: "))
+# total=1
+# for i in range(sumatoria):
+#        total=total*(i+1)
+# print("La sumatoria es: ", total)
+
+#Usando string en vez de numeros
+# for i in "Vicente":
+#     print(i)
+
+# letras=0
+# nombre=input("Ingrese su nombre: ")
+# for i in nombre:
+#     print(i)
+#     letras=letras+1
+# print(f"El total de letras es {letras}")
+
+
+#Contador de vocales y consonantes
+# vocales=0
+# conso=0
+# nombre=input("Ingrese su nombre: ")
+# for i in nombre:
+#     print(i)
+#     if i in "aeiouAEIOU":
+#         vocales=vocales+1
+#     elif i ==" ":
+#         ''''''
+#     else:
+#         conso+=1
+
+# print(f"El total de vocales es {vocales}")
+# print(f"El total de consonantes es {conso}")
